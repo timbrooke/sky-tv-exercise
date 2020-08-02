3e59b915108d57172ed5bf5f5dd4a36928766cac
Tim Brooke <timlbrooke@gmail.com>
2020-08-02 22:23:54 +0100

    adds 2nd face tracker

--- src/pages/monster.js
+++ src/pages/monster.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { initApp } from '../apps/comedyGlasses/comedyGlasses';
 
 const MonsterPage = () => {
-  return <div>Monster</div>;
+  useEffect(()=>{
+    initApp();
+  })
+  return (
+    <div className="App">
+      <canvas width="1024" height="1024" id='jeeFaceFilterCanvas'/>
+      <div id='jeelizFaceFilterFollow'/>
+    </div>
+  );
 };
+
+
 
 export default MonsterPage;
